1d3738f17816368e3e4f616dc686c36359d4d7ce
Jufino <jufo@altas.sk>
2013-07-14 22:29:44 +0000

    version 1.0

---
+++ serial.h
@@ -0,0 +1,22 @@
+#ifndef _SERIAL_H
+#define _SERIAL_H
+
+#include <string.h> // string function definitions
+#include <unistd.h> // UNIX standard function definitions
+#include <fcntl.h> // File control definitions
+#include <termios.h> // POSIX terminal control definitionss
+#include <time.h>   // time calls
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+//--------------------------------------------------------
+int SerialOpen(const char *port, speed_t speed);
+//-----------------------------------------
+void SerialClose(int port);
+//-----------------------------------------
+void SerialWrite(int port,char data[]);
+//------------------------------------------
+char *SerialReadLine(int port, int max);
+//------------------------------------------
+char *SerialRead(int port,int pocet);
+#endif
